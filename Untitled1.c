405f3e0680cfe4f8cd6cd33e907c55e99fa03700
Venktesh Naidu <venkteshn@kpit.com>
2018-09-27 15:32:51 +0530

    Initial commit

---
+++ Untitled1.c
@@ -0,0 +1,196 @@
+#include <LiquidCrystal.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#define SET_bit(PORT,BIT)  PORT|=(1<<BIT)
+#define CLR_bit(PORT,BIT)  PORT&=~(1<<BIT)
+struct
+{
+  volatile unsigned int ISR1:1;
+}FLAG;
+
+const int pingPin = 7;//configuring as input pin for sensor
+
+long readUltrasonic(int pin)
+
+{
+
+  SET_bit(DDRD,pin);
+
+  CLR_bit(PORTD,pin);
+
+  _delay_ms(2);
+
+  SET_bit(PORTD,pin);
+
+  _delay_ms(5);
+
+  CLR_bit(PORTD,pin);
+
+  CLR_bit(DDRD,pin);
+
+  return pulseIn(pin, HIGH);
+
+}
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / 29 / 2;
+}
+
+int main(void)
+{
+  long  inches, cm;
+  Serial.begin(9600);
+  SET_bit(DDRB,PB0);
+  CLR_bit(DDRD,PD7);//  sensor
+  CLR_bit(DDRD,PD2);//ignition switch
+
+
+  EICRA|=(1<<ISC00);
+  EICRA&=~(1<<ISC01);// Any logical change will raise interrupt
+  EIMSK|=(1<<INT0);//Local Interrupt enable
+  sei();//
+
+while(1)
+{
+if(FLAG.ISR1==1)
+{
+  cm = microsecondsToCentimeters(readUltrasonic(7));
+if(PIND & (1<<PD2))
+          {
+            CLR_bit(PORTB,PB0);
+          }
+else if(PIND & ~(1<<PD2))
+{
+
+  if(cm<250)//buzzer range
+  {
+    SET_bit(PORTB,PB0);
+  }
+
+  else
+  {
+    CLR_bit(PORTB,PB0);
+  }
+
+}
+
+
+ }
+}
+          }
+ISR(INT0_vect)  //interrupt for switch
+            {
+              cli();
+            FLAG.ISR1=1;
+              sei();
+            }
+
+
+
+
+Regards,
+Venktesh Naidu
+Trainee | ADAS
+KPIT Technologies Limited, Bengaluru
+Cell: +918861974844
+
+From: Rayeesa Momin
+Sent: Friday, September 14, 2018 3:57 PM
+To: Venktesh Naidu
+Subject:
+
+#include <LiquidCrystal.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <stdint.h>
+#define SET_bit(PORT,BIT)  PORT|=(1<<BIT)
+#define CLR_bit(PORT,BIT)  PORT&=~(1<<BIT)
+
+const uint8_t pingPin = 7;
+struct
+{
+  volatile unsigned int flag:1;
+}flag_bit;
+
+
+long readUltrasonicDistance(int pin)  //function to  read sensor
+
+{
+
+  SET_bit(DDRD,pin);
+
+  CLR_bit(PORTD,pin);
+
+  //_delay_ms(2);
+
+  SET_bit(PORTD,pin);
+
+// _delay_ms(5);
+
+  CLR_bit(PORTD,pin);
+
+  CLR_bit(DDRD,pin);
+
+  return pulseIn(pin, HIGH);
+
+}
+
+
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / 29 / 2;
+}
+
+int main(void)
+{
+  float inches, cm;
+  SET_bit(DDRB,PB0);//8th pin buzzer
+  CLR_bit(DDRD,PD7);//SENSOR signal PIN
+  SET_bit(DDRB,PB1);//9th pin LED
+  CLR_bit(PORTB,PB1);
+
+
+                CLR_bit(DDRD,PD2);                                       // 2nd pin for switch
+
+                EICRA|=(1<<ISC00);
+    EICRA|=(1<<ISC01); // Any logical change will raise interrupt
+    EIMSK|=(1<<INT0); //Local Interrupt enable
+    sei();
+while(1)
+{
+  cm = microsecondsToCentimeters(readUltrasonicDistance(pingPin));
+
+if(flag_bit.flag==1)//to check switch(engine) on condition
+
+{
+if(cm<25)        //too close ,buzzer on
+{
+SET_bit(PORTB,PB0);
+
+
+}
+else if(cm<50 && cm>25)// within specified range ,warning led
+{
+//CLR_bit(PORTB,PB0);
+SET_bit(PORTB,PB1);
+}
+else            // led and buzzer off
+{
+  CLR_bit(PORTB,PB1);
+  CLR_bit(PORTB,PB0);
+
+
+}
+}
+}
+}
+
+ISR(INT0_vect)  //interrupt for switch
+            {
+                                                cli();
+            flag_bit.flag=1;
+                                                sei();
+            }
+
